46b70d541fb1837246c5572e1b2641bad7cc5c7c
Feitas_tony <gm@wffeitas.com>
2017-03-05 11:02:45 +0800

    Add files via upload

--- models/models.py
+++ models/models.py
@@ -6,13 +6,13 @@ from odoo import models, fields, api
 class BenchMarketing(models.Model):
     _name = 'bench.marketing'
 
-    name = fields.Char(stirng="Name")
+    name = fields.Char(stirng="Name", required=1)
     type = fields.Selection([('inter','Internal'),('comp','Competitive'),('func','Functional or industry')],
     	string="Type")
     responsible = fields.Many2one("res.users", string="Responsible")
     description = fields.Text()
     state = fields.Selection([('draft','Draft'),('analyse','Analyse'),('action','Action'),('done','Done')],
-    	string="State")
+    	string="State", default='draft')
 
     metrics_ids = fields.One2many("bench.marketing.metrics", "benchmarketing_id")
     log_ids = fields.One2many("bench.marketing.log", "benchmarketing_id")
@@ -21,8 +21,8 @@ class BenchMarketing(models.Model):
 class BenchMarketingMetrics(models.Model):
     _name = 'bench.marketing.metrics'
 
-    name = fields.Char()
-    description = fields.Text()
+    name = fields.Char(string="Name", required=1)
+    description = fields.Text(string="Description")
     benchmarketing_id = fields.Many2one("bench.marketing", "BenchMarketing")
     team_id = fields.Many2one("bench.marketing.team")
     target_id = fields.Many2one("bench.marketing.target")
@@ -31,23 +31,23 @@ class BenchMarketingMetrics(models.Model):
 class BenchMarketingTeam(models.Model):
     _name = 'bench.marketing.team'
 
-    name = fields.Char(string="Name")
+    name = fields.Char(string="Name", required=1)
     responsible = fields.Many2one("res.users", string="Leader")
-    description = fields.Text()
+    description = fields.Text(string="Description")
 
 
 class BenchMarketingLog(models.Model):
     _name = 'bench.marketing.log'
 
-    name = fields.Char()
-    description = fields.Text()
+    name = fields.Char(string="Name")
+    description = fields.Text(string="Description")
     benchmarketing_id = fields.Many2one("bench.marketing", "BenchMarketing")
 
 
 class BenchMarketingTarget(models.Model):
     _name = 'bench.marketing.target'
 
-    name = fields.Char()
+    name = fields.Char(string="Name", required=1)
     partner_id = fields.Many2one("res.partner", string="Standard Object")
     content = fields.Text(string="Content")
     scheme = fields.Text(string="Scheme")
